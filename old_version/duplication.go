5d58b23c322c57949df28a782c60186f80205f8e
Alice <jingyu.zhang@flex-solver.com.sg>
2021-03-17 23:14:35 +0800

    duplication code follows format now

--- old_version/duplication.go
+++ old_version/duplication.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"time"
-	// "os"
-	// "strconv"
+	"os"
+	"strconv"
 )
 
 type Message struct{
@@ -23,42 +23,33 @@ type Node struct{
 }
 
 func main() {
-	// arg := os.Args[1] //get the number of nodes
-	// numNodes, _ := strconv.Atoi(arg) //set this as the number of nodes we have
+	arg := os.Args[1] //get the number of nodes
+	numNodes, _ := strconv.Atoi(arg) //set this as the number of nodes we have
 
 	
-	// allNodes := make([]Node, numNodes) //make the nodes
+	allNodes := make([]Node, numNodes) //make the nodes
 
-	// for i:= 0; i< numNodes; i++{
-	// 	allNodes[i].id = i
-	// 	allNodes[i].revChan = make(chan Message) //from master server to the server
-	// 	allNodes[i].replyChan = make(chan bool) //from servers to master
-	// 	allNodes[i].quitElect = make(chan int)
-	// 	allNodes[i].killNode = make(chan int)
-	// 	allNodes[i].peers = allNodes
-	// 	allNodes[i].coordinator = numNodes-1 
-	// }
+	for i:= 0; i< numNodes; i++{
+		allNodes[i].id = i
+		allNodes[i].revChan = make(chan Message) //from master server to the server
+		allNodes[i].replyChan = make(chan bool) //from servers to master
+		// allNodes[i].quitElect = make(chan int)
+		// allNodes[i].killNode = make(chan int)
+		// allNodes[i].peers = allNodes
+		// allNodes[i].coordinator = numNodes-1 
+	}
 	//channels between master server and client
 	fromClient := make(chan Message)
 	toClient := make(chan bool)
-	//channels from master server [0] to other servers in cell [1-4]
-	M1 := make(chan Message)
-	M2 := make(chan Message)
-	M3 := make(chan Message)
-	M4 := make(chan Message)
-	//channels from servers in cell [1-4] to master server [0]
-	S1 := make(chan bool)
-	S2 := make(chan bool)
-	S3 := make(chan bool)
-	S4 := make(chan bool)
 
 	go clientWriteReq("new file content!", "FileABC", fromClient)
-	go masterPropagate(fromClient, M1, M2, M3, M4, S1, S2, S3, S4, toClient)
-	go cellServerOK(1, M1, S1)
-	go cellServerOK(2, M2, S2)
-	go cellServerOK(3, M3, S3)
-	//go cellServerOK(4, M4, S4)
-	go cellServerFail(4, M4, S4)
+	go masterPropagate(fromClient, allNodes, toClient)
+
+	for i:= 0; i< len(allNodes)-1; i++{
+		go cellServerOK(i+1, allNodes[i].revChan, allNodes[i].replyChan)
+	}
+
+	go cellServerFail(4, allNodes[len(allNodes)-1].revChan, allNodes[len(allNodes)-1].replyChan)
 	go clientAwaitACK(toClient)
 
 	//take grace period as 15 seconds, if not reponse close program
@@ -72,44 +63,32 @@ func clientWriteReq(fileContent string, fileName string, fromClient chan Message
 	fromClient <- data
 }
 
-func masterPropagate(fromClient chan Message, M1 chan Message, M2 chan Message, M3 chan Message, M4 chan Message, S1 chan bool, S2 chan bool, S3 chan bool, S4 chan bool, toClient chan bool) {
+func masterPropagate(fromClient chan Message, allNodes []Node, toClient chan bool) {
 	for {
 		data := <-fromClient
 		fileName := data.fileName
 		fileContent := data.fileContent
 		fmt.Println("server has received request to write to", fileName)
-		M1 <- data
-		M2 <- data
-		M3 <- data
-		M4 <- data
+		for i:=0; i< len(allNodes); i++{
+			allNodes[i].revChan <- data
+		}
 		fmt.Println("server has propagated request to write to entire cell, awaiting response...")
 
 		//receiving responses from servers in cell
 		//master wait for servers to repsond w true, else will not continue
 
-		ACK1 := <-S1
-		ACK2 := <-S2
-		ACK3 := <-S3
-		ACK4 := <-S4
+		OK := true
+		for i:=0; i<len(allNodes); i++{
+			checkBool := <- allNodes[i].replyChan
+			if checkBool != true{
+				fmt.Println("error msg from server ", i+ 1, "\n")
+				OK = false
+			}
 
-		OK := ACK1 && ACK2 && ACK3 && ACK4 //if all ACKS are received, OK == 1 
-
-		if !ACK1 {
-			fmt.Println("error msg from server 1")
 		}
-		if !ACK2 {
-			fmt.Println("error msg from server 2")
-		}
-		if !ACK3 {
-			fmt.Println("error msg from server 3")
-		}
-		if !ACK4 {
-			fmt.Println("error msg from server 4")
-		}
-
-		//master received all ACKS
-		fmt.Println("master has received ACK from all servers")
+		
 		if OK {
+			fmt.Println("master has received ACK from all servers")
 			//master updating database
 			time.Sleep(5 * time.Second)
 			fmt.Println("master finished updating", fileName, "w", fileContent)
