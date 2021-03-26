80ca1b65f76eee04801b69c3902f349d85bc6fcf
Wang Zefan <zefan_wang@mymail.sutd.edu.sg>
2021-03-26 11:15:30 +0800

    Added client keepalives

--- docker/client/rpc_client.go
+++ docker/client/rpc_client.go
@@ -1,28 +1,71 @@
 package main
+
 import (
-   "log"
-   "net/rpc"
+	"fmt"
+	"log"
+	"net/rpc"
+	"time"
 )
+
 type Reply struct {
-   Data string
+	Data string
 }
+
+type Message struct {
+	SenderID int
+	Msg      string
+}
+
+type Client struct {
+	id          int
+	Coordinator int
+	rpcChan     *rpc.Client
+	all_ip      [3]string
+}
+
 func main() {
-  log.Printf("Client is created")
-  client, err := rpc.Dial("tcp", "172.22.0.3:1234")
-  if err != nil {
-    log.Fatal(err)
-  }
-  for {
-    line := "hello"
-    if err != nil {
-       log.Fatal(err)
-    }
-    var reply Reply
-    err = client.Call("Listener.GetLine", line, &reply)
-    if err != nil {
-      log.Fatal(err)
-    }
-    log.Printf("Reply: %v, Data: %v", reply, reply.Data)
-    break
-  }
+
+	//TODO: Client needs to communicate with chubby cell to find out coordinator
+
+	log.Printf("Client is created")
+
+	client := Client{id: 0, Coordinator: 2, all_ip: [3]string{"172.22.0.7:1234", "172.22.0.3:1234", "172.22.0.4:1234"}}
+	clientChan, err := rpc.Dial("tcp", client.all_ip[2])
+	if err != nil {
+		log.Fatal("client connection with server 2 error")
+	}
+	client.rpcChan = clientChan
+
+	// Client first exchange keepalives with Node/Server 2
+	for {
+		//keepalive
+		var KeepAliveReply Reply
+		keepalive_err := client.rpcChan.Call("Listener.Keepalive", &client, &KeepAliveReply)
+		time.Sleep(time.Second * 5)
+		log.Printf(KeepAliveReply.Data)
+		if keepalive_err != nil {
+			// TODO: Client needs to communicate with chubby cell to find out coordinator
+			fmt.Println("server node died")
+			break
+		}
+	}
+
+	// TODO: Client needs to first communicate with chubby cell to find out new coordinator
+	// Client then exchange keepalives with Node/Server 1
+	for {
+		var KeepAliveReply Reply
+		clientChan, err := rpc.Dial("tcp", client.all_ip[1])
+		if err != nil {
+			log.Fatal("client connection with server 1 error")
+		}
+		client.rpcChan = clientChan
+		client.Coordinator = 1
+		keepalive_err := client.rpcChan.Call("Listener.Keepalive", &client, &KeepAliveReply)
+		if keepalive_err != nil {
+			log.Fatal(keepalive_err)
+			log.Fatal("server node died again")
+		}
+		time.Sleep(time.Second * 5)
+		log.Printf(KeepAliveReply.Data)
+	}
 }
--- docker/server/rpc_server.go
+++ docker/server/rpc_server.go
@@ -1,143 +1,160 @@
 package main
+
 import (
-   "fmt"
-   "log"
-   "net"
-   "net/rpc"
-   "os"
-   "strconv"
-   "time"
+	"fmt"
+	"log"
+	"net"
+	"net/rpc"
+	"os"
+	"strconv"
+	"time"
 )
+
 type Listener int
 
 type Reply struct {
-   Data string
+	Data string
 }
 
-type Message struct{
-   SenderID int
-   Msg string
+type Message struct {
+	SenderID int
+	Msg      string
 }
 
-type Node struct{
-   id int
-   all_ip [3]string
-   Coordinator int
-   rpcChan *rpc.Client
-   // revChan chan Message
-   // replyChan chan Message
-   // ClientChan chan c.SelfIntroduction
-   // peers []Node
-   // quitElect chan int
-   // killNode chan int
-   // AllClients []chan c.File
-   // FileChan chan c.File
-   // database map[string]Lock
+type Client struct {
+	id               int
+	Coordinator      int
+	rpcChan          *rpc.Client
+	keepAliveRequest bool
+}
+
+type Node struct {
+	id          int
+	all_ip      [3]string
+	Coordinator int
+	rpcChan     *rpc.Client
+	// revChan chan Message
+	// replyChan chan Message
+	// ClientChan chan c.SelfIntroduction
+	// peers []Node
+	// quitElect chan int
+	// killNode chan int
+	// AllClients []chan c.File
+	// FileChan chan c.File
+	// database map[string]Lock
 }
 
 func (l *Listener) GetLine(msg Message, reply *Reply) error {
-   // rv := string(line)
-   fmt.Printf("Received: %v from server %v\n\n", msg.Msg, msg.SenderID)
-   concat := "received: "+msg.Msg
-   *reply = Reply{concat}
-   return nil
+	// rv := string(line)
+	fmt.Printf("Received: %v from server %v\n\n", msg.Msg, msg.SenderID)
+	concat := "received: " + msg.Msg
+	*reply = Reply{concat}
+	return nil
+}
+
+func (l *Listener) Keepalive(c *Client, reply *Reply) error {
+	fmt.Printf("Received: KeepAlive Request from client %v\n\n", c.id)
+	concat := "Received KeepAlive reply from server " + strconv.Itoa(c.Coordinator)
+	*reply = Reply{concat}
+	return nil
 }
 
 func makeNode(id int) *Node {
-   all_ip := [3]string{"172.22.0.7:1234", "172.22.0.3:1234", "172.22.0.4:1234"}
-   Coordinator := 2
-   curr_node := Node{id, all_ip, Coordinator, nil}
+	all_ip := [3]string{"172.22.0.7:1234", "172.22.0.3:1234", "172.22.0.4:1234"}
+	Coordinator := 2
+	curr_node := Node{id, all_ip, Coordinator, nil}
 
-   // allNodes[i].revChan = make(chan Message)
-   // allNodes[i].replyChan = make(chan Message)
-   // allNodes[i].quitElect = make(chan int)
-   // allNodes[i].killNode = make(chan int)
-   // allNodes[i].peers = allNodes
-   // allNodes[i].Coordinator = numNodes - 1
-   // allNodes[i].ClientChan = make(chan c.SelfIntroduction)
-   // allNodes[i].database = make(map[string]Lock)
+	// allNodes[i].revChan = make(chan Message)
+	// allNodes[i].replyChan = make(chan Message)
+	// allNodes[i].quitElect = make(chan int)
+	// allNodes[i].killNode = make(chan int)
+	// allNodes[i].peers = allNodes
+	// allNodes[i].Coordinator = numNodes - 1
+	// allNodes[i].ClientChan = make(chan c.SelfIntroduction)
+	// allNodes[i].database = make(map[string]Lock)
 
-   // for i := 0; i < numNodes; i++ {
-   //    go allNodes[i].ping()
-   //    go allNodes[i].checkChannel()
-   // }
+	// for i := 0; i < numNodes; i++ {
+	//    go allNodes[i].ping()
+	//    go allNodes[i].checkChannel()
+	// }
 
-   go curr_node.ping()
+	go curr_node.ping()
 
-   return &curr_node
+	return &curr_node
 }
 
-func send(msg Message, rpcChan *rpc.Client, send_id int) string{
-   var reply Reply
-   err := rpcChan.Call("Listener.GetLine", msg, &reply)
-   if err != nil {
-      if err.Error() == "connection is shut down"{
-         log.Println("yes")
-      }
-      log.Printf("type: %T\n", err)
-      // log.Fatal(err)
-   }
-   log.Println("Does it reach here?")
-   log.Printf("Reply: %v, Data: %v\n", reply, reply.Data)
-   return reply.Data
+func send(msg Message, rpcChan *rpc.Client, send_id int) string {
+	var reply Reply
+	err := rpcChan.Call("Listener.GetLine", msg, &reply)
+	if err != nil {
+		if err.Error() == "connection is shut down" {
+			log.Println("yes")
+		}
+		log.Printf("type: %T\n", err)
+		// log.Fatal(err)
+	}
+	log.Println("Does it reach here?")
+	log.Printf("Reply: %v, Data: %v\n", reply, reply.Data)
+	return reply.Data
 }
 
 func (n *Node) ping() {
-   if n.Coordinator == n.id{
-      return
-   }
+	if n.Coordinator == n.id {
+		return
+	}
 
-   // Establish connection with new Coordinator
-   server, err := rpc.Dial("tcp", n.all_ip[n.Coordinator])
-   if err != nil {
-    log.Fatal(err)
-   }
-   n.rpcChan = server
+	// Establish connection with new Coordinator
+	server, err := rpc.Dial("tcp", n.all_ip[n.Coordinator])
+	if err != nil {
+		log.Fatal(err)
+	}
+	n.rpcChan = server
 
-   log.Println("Server", n.id, "is pinging Coordinator:", n.Coordinator)
-   // Start connection protocol
-   for{
-      <-time.After(5*time.Second)
-      reply := send(Message{n.id, "ping"}, n.rpcChan, n.Coordinator)
-      log.Printf("Received reply from Coordinator: %v", reply)
-   }
-   // Close the connection to server
-   n.rpcChan.Close()
-   // for {
-   //    // fmt.Println("peers: ", n.peers)
-   //    fmt.Println("node: ", n.id, "is pinging master server: ", n.Coordinator)
-   //    select{
-   //    case n.peers[n.Coordinator].revChan<-Message{n.id, "Are you alive?"}:
-   //       time.Sleep(time.Second * 3)
-   //    case <-time.After(time.Second * 10):
-   //       n.elect()
-   //    case <-n.killNode:
-   //       return
-   //    }
-   // }
+	log.Println("Server", n.id, "is pinging Coordinator:", n.Coordinator)
+	// Start connection protocol
+	for {
+		<-time.After(5 * time.Second)
+		reply := send(Message{n.id, "ping"}, n.rpcChan, n.Coordinator)
+		log.Printf("Received reply from Coordinator: %v", reply)
+	}
+	// Close the connection to server
+	n.rpcChan.Close()
+	// for {
+	//    // fmt.Println("peers: ", n.peers)
+	//    fmt.Println("node: ", n.id, "is pinging master server: ", n.Coordinator)
+	//    select{
+	//    case n.peers[n.Coordinator].revChan<-Message{n.id, "Are you alive?"}:
+	//       time.Sleep(time.Second * 3)
+	//    case <-time.After(time.Second * 10):
+	//       n.elect()
+	//    case <-n.killNode:
+	//       return
+	//    }
+	// }
 }
 
 func main() {
-   // addy, err := net.ResolveTCPAddr("tcp", "172.20.0.2:12345")
-   // if err != nil {
-   //   log.Fatal(err)
-   // }
+	// addy, err := net.ResolveTCPAddr("tcp", "172.20.0.2:12345")
+	// if err != nil {
+	//   log.Fatal(err)
+	// }
 
-   // Get ip addr
-   ip_addr := os.Args[1]
-   id_arg := os.Args[2]
-   id, _ := strconv.Atoi(id_arg)
+	// Get ip addr
+	ip_addr := os.Args[1]
+	id_arg := os.Args[2]
+	id, _ := strconv.Atoi(id_arg)
 
-   log.Println("Server", id, "is running")
+	log.Println("Server", id, "is running")
 
-   go makeNode(id)
+	go makeNode(id)
 
-   // inbound, err := net.Listen("tcp", "172.22.0.3:1234")
-   inbound, err := net.Listen("tcp", ip_addr)
-   if err != nil {
-     log.Fatal(err)
-   }
-   listener := new(Listener)
-   rpc.Register(listener)
-   rpc.Accept(inbound)
+	// inbound, err := net.Listen("tcp", "172.22.0.3:1234")
+	inbound, err := net.Listen("tcp", ip_addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	listener := new(Listener)
+	rpc.Register(listener)
+	rpc.Accept(inbound)
+
 }
